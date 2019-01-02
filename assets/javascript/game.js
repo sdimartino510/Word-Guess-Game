c31c748a49384439a14dd1c0ffae892acbcf5622
ArchangelD <sdimartino@gmail.com>
2019-01-01 21:27:03 -0800

    Changes size of noose image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -160,21 +160,3 @@ function isGameOver() {
 // function updateDOM() {
 //     document.getElementById("wins").innerHTML("Wins: " + numberOfWins);
 // }
-
-//Browser generates random word from pre-determined list (array) and generates an underscore for each letter.
-    
-//Player is given ten opportunities to guess letters
-
-//Player selects a letter by pressing that letter key on keyboard
-
-//Guessed letter is compared to letters in selected word
-
-//If letter is in selected word, letter appears in corresponding place in word (replacing underscore), player can no longer guess that letter, and player continues guessing
-
-//If letter is not in selected word, player loses one of their ten guesses, incorrectly guessed letter appears in "letters already guessed," and player can no longer guess that letter.
-
-//If player correctly guesses all letters in selected word within the ten guess limit, alert "You win!"
-
-//If player uses up all ten guesses without guessing word, alert "Sorry, you lose."
-
-//When player acknowledges alert box, game resets, another word is randomly chosen from array, and play begins again.
